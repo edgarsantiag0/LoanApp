29a67d4ad65aae262be4186b9d0ead0199323892
Edgar Santiago <itt.edgarstgo@gmail.com>
2022-05-03 19:26:22 -0400

    Add project files.

---
+++ LoanApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LoanApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
